2bc9cf473ee91e90d404ef252bdc8123ab0c0bc5
AmirSa12 <amirhosseinpr184@gmail.com>
2023-11-26 18:42:41 +0330

    btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="w-[100%] h-[100vh] border-2 border-solid border-red-500">
       <div>
         <h1 className="text-center">Quiz App | Start a Random Quiz</h1>
+        <div className="w-fit text-center mx-auto cursor-pointer border-2 border-solid border-black-500 p-[10px]">
+          Start A Random Quiz
+        </div>
       </div>
     </div>
   );
