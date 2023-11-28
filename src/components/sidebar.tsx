00bd6ee0aa9a7b45c32f5f0a3209e38ccbda23ef
amirhossain <zohrabiamirhossain@gmail.com>
2023-11-28 22:40:59 +0330

    ui update

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
           OR
         </button>
       </div>
-      <div className="w-[92%] h-[25rem] bg-red-300 flex self-center flex-col rounded-[5px]">
+      <div className="w-[92%] h-[30rem] bg-red-300 flex self-center flex-col rounded-[5px]">
         <h1 className="text-center mt-3 mb-2 text-2xl font-semibold">categories</h1>
         <div className="gap-[5px] grid auto-rows-auto w-[100%] grid-cols-3 h-[9rem] pl-2 mt-2">
           <div className="w-[5rem] h-[2.5rem] bg-yellow-400 rounded-[5px]"><p>rem</p></div>
@@ -22,7 +22,7 @@ export default function Sidebar() {
           <div className="w-[5rem] h-[2.5rem] bg-yellow-400 rounded-[5px]"><p>rem</p></div>
           <div className="w-[5rem] h-[2.5rem] bg-yellow-400 rounded-[5px]"><p>rem</p></div>
         </div>
-        <div className="flex flex-col mt-[.7rem] ">
+        <div className="flex flex-col mt-[.7rem] ml-2">
           <h2 className="text-center text-2xl font-semibold">difficality</h2>
           <div className="grid grid-cols-3 mt-3">
             <div className="w-[5rem] h-[2.5rem] bg-yellow-400 rounded-[5px]"><p>rem</p></div>
@@ -34,6 +34,9 @@ export default function Sidebar() {
           <h1 className="text-center mb-2 text-2xl font-semibold">limit</h1>
           <input type="range" min={0} max={40} value={0} />
         </div>
+        <button className="w-[14rem] h-[3.5rem] bg-teal-800 rounded-[10px] flex self-center mt-4">
+          <h1 className="text-center ">search</h1>
+        </button>
       </div>
     </div>
   )
